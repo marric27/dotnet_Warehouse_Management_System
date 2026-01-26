5c769732befdff6645db926d18f8a7166a85569e
Marco Ricci <marco.ricci796@gmail.com>
2026-01-26 17:08:46 +0100

    implement product unit tests

--- Picking/Services/PickingService.cs
+++ Picking/Services/PickingService.cs
@@ -12,7 +12,7 @@ namespace dotnet_Warehouse_Management_System.Picking.Services
     {
         private readonly IPicklistService _picklistService;
         private readonly IPickingInfoService _pickingInfoService;
-        private readonly IStockUnitService _stockUnitService;
+        //private readonly IStockUnitService _stockUnitService;
         public PickingService(IPicklistService picklistService, IPickingInfoService pickingInfoService)
         {
             _picklistService = picklistService;
@@ -21,130 +21,130 @@ namespace dotnet_Warehouse_Management_System.Picking.Services
 
         public Task<PicklistItemDto> GetNextPickListItem(NextItemRequest nextItemRequest) => _picklistService.GetNextPickListItemAsync(nextItemRequest);
 
-        public void ConfirmPicking(ConfirmPickingRequest request)
-        {
-            // carico i picklist item dati plcode e plitemcode
-            PicklistItemDto picklistItem = LoadPickListItem(request.PickListCode, request.PickListItemCode);
-            Dictionary<string, int> stockUnitQuantities = request.stockUnitQuantities
-                .ToDictionary(
-                    x => x.SuId,
-                    x => x.Quantity
-                );
-            if (stockUnitQuantities == null || stockUnitQuantities.Count == 0)
-            {
-                throw new Exception("No stock units provided for picking");
-            }
-            int toPick = stockUnitQuantities.Values.Sum();
-            if(toPick > picklistItem.Quantity - picklistItem.PickedQty) throw new Exception("Errore: Stai richiedendo quantità maggiore di quanto specificata nel pick list item");
+        //public void ConfirmPicking(ConfirmPickingRequest request)
+        //{
+        //    // carico i picklist item dati plcode e plitemcode
+        //    PicklistItemDto picklistItem = LoadPickListItem(request.PickListCode, request.PickListItemCode);
+        //    Dictionary<string, int> stockUnitQuantities = request.stockUnitQuantities
+        //        .ToDictionary(
+        //            x => x.SuId,
+        //            x => x.Quantity
+        //        );
+        //    if (stockUnitQuantities == null || stockUnitQuantities.Count == 0)
+        //    {
+        //        throw new Exception("No stock units provided for picking");
+        //    }
+        //    int toPick = stockUnitQuantities.Values.Sum();
+        //    if(toPick > picklistItem.Quantity - picklistItem.PickedQty) throw new Exception("Errore: Stai richiedendo quantità maggiore di quanto specificata nel pick list item");
 
-            ErrorReason? errorReason;
-            if (toPick < picklistItem.PickedQty && request.ErrorReason != null)
-            {
-                errorReason = request.ErrorReason.Value;
-            }
-            else if (toPick == picklistItem.Quantity)
-            {
-                errorReason = null;
-            }
-            else
-            {
-                throw new Exception("Error reason cant be omitted when qty to pick is lower than ");
-            }
+        //    ErrorReason? errorReason;
+        //    if (toPick < picklistItem.PickedQty && request.ErrorReason != null)
+        //    {
+        //        errorReason = request.ErrorReason.Value;
+        //    }
+        //    else if (toPick == picklistItem.Quantity)
+        //    {
+        //        errorReason = null;
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Error reason cant be omitted when qty to pick is lower than ");
+        //    }
 
-            Dictionary<string, StockUnitDto> StockUnitsByCode = [];
-            foreach (string code in stockUnitQuantities.Keys)
-            {
-                StockUnitDto stockUnit = StockUnitService.GetByCode(code);
-                StockUnitsByCode.Add(stockUnit.Code, stockUnit);
-            }
+        //    Dictionary<string, StockUnitDto> StockUnitsByCode = [];
+        //    foreach (string code in stockUnitQuantities.Keys)
+        //    {
+        //        StockUnitDto stockUnit = StockUnitService.GetByCode(code);
+        //        StockUnitsByCode.Add(stockUnit.Code, stockUnit);
+        //    }
 
-            CanPickFromSU(stockUnitQuantities, StockUnitsByCode, picklistItem);
+        //    CanPickFromSU(stockUnitQuantities, StockUnitsByCode, picklistItem);
 
-            ExecutePicking(stockUnitQuantities, StockUnitsByCode, picklistItem);
+        //    ExecutePicking(stockUnitQuantities, StockUnitsByCode, picklistItem);
 
-            UpdatePicklistItem(picklistItem, toPick, errorReason);
+        //    UpdatePicklistItem(picklistItem, toPick, errorReason);
 
 
-        }
+        //}
 
-        private void UpdatePicklistItem(PicklistItemDto picklistItem, int toPick, ErrorReason? errorReason)
-        {
-            throw new NotImplementedException();
-        }
+        //private void UpdatePicklistItem(PicklistItemDto picklistItem, int toPick, ErrorReason? errorReason)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private void ExecutePicking(Dictionary<string, int> requested, Dictionary<string, StockUnitDto> stockUnitsByCode, PicklistItemDto picklistItem)
-        {
-            foreach( var entry in requested)
-            {
-                string code = entry.Key;
-                int quantityToPick = entry.Value;
+        //private void ExecutePicking(Dictionary<string, int> requested, Dictionary<string, StockUnitDto> stockUnitsByCode, PicklistItemDto picklistItem)
+        //{
+        //    foreach( var entry in requested)
+        //    {
+        //        string code = entry.Key;
+        //        int quantityToPick = entry.Value;
 
-                StockUnitDto su = stockUnitsByCode[code];
-                int oldQty = su.Quantity;
-                su.Quantity = oldQty - quantityToPick;
-                _stockUnitService.UpdateAsync(su.Id, su);
+        //        StockUnitDto su = stockUnitsByCode[code];
+        //        int oldQty = su.Quantity;
+        //        su.Quantity = oldQty - quantityToPick;
+        //        _stockUnitService.UpdateAsync(su.Id, su);
 
-                CreatePickingInfo(su, quantityToPick, picklistItem);
+        //        CreatePickingInfo(su, quantityToPick, picklistItem);
 
-            }
-        }
+        //    }
+        //}
 
-        private void CreatePickingInfo(StockUnitDto su, int pickedQty, PicklistItemDto picklistItem)
-        {
-            PickingInfoDto pickingInfo = new PickingInfoDto()
-            {
-                User = "USR-01QWERTY",
-                Timestamp = DateTime.Now,
-                Quantity = pickedQty,
-                StockUnitCode = su.Code,
-                BatchNumber = su.BatchNumber,
-                ExpirationDate = su.ExpirationDate,
+        //private void CreatePickingInfo(StockUnitDto su, int pickedQty, PicklistItemDto picklistItem)
+        //{
+        //    PickingInfoDto pickingInfo = new PickingInfoDto()
+        //    {
+        //        User = "USR-01QWERTY",
+        //        Timestamp = DateTime.Now,
+        //        Quantity = pickedQty,
+        //        StockUnitCode = su.Code,
+        //        BatchNumber = su.BatchNumber,
+        //        ExpirationDate = su.ExpirationDate,
                 
-            };
-            var created = _pickingInfoService.CreateAsync(pickingInfo);
-        }
+        //    };
+        //    var created = _pickingInfoService.CreateAsync(pickingInfo);
+        //}
 
-        private void CanPickFromSU(Dictionary<string, int> requested, Dictionary<string, StockUnitDto> stockUnitsByCode, PicklistItemDto picklistItem)
-        {
-            foreach(var entry in requested)
-            {
-                string code = entry.Key;
-                int quantity = entry.Value;
+        //private void CanPickFromSU(Dictionary<string, int> requested, Dictionary<string, StockUnitDto> stockUnitsByCode, PicklistItemDto picklistItem)
+        //{
+        //    foreach(var entry in requested)
+        //    {
+        //        string code = entry.Key;
+        //        int quantity = entry.Value;
 
-                if (!stockUnitsByCode.TryGetValue(code, out var su))
-                {
-                    throw new KeyNotFoundException($"StockUnit {code} not found in stockUnits dictionary.");
-                }
+        //        if (!stockUnitsByCode.TryGetValue(code, out var su))
+        //        {
+        //            throw new KeyNotFoundException($"StockUnit {code} not found in stockUnits dictionary.");
+        //        }
 
-                if (!su.ProductCode.Equals(picklistItem.productCode, StringComparison.OrdinalIgnoreCase))
-                {
-                    throw new Exception(
-                        $"StockUnit {code} contains product {su.ProductCode} " +
-                        $"but PickListItem requires product {picklistItem.productCode}"
-                    );
-                }
+        //        if (!su.ProductCode.Equals(picklistItem.productCode, StringComparison.OrdinalIgnoreCase))
+        //        {
+        //            throw new Exception(
+        //                $"StockUnit {code} contains product {su.ProductCode} " +
+        //                $"but PickListItem requires product {picklistItem.productCode}"
+        //            );
+        //        }
 
-                if (quantity > su.Quantity)
-                {
-                    throw new Exception(
-                        $"Requested quantity {quantity} > available quantity {su.Quantity} for stock unit: {code}"
-                    );
-                }
-            }
-        }
+        //        if (quantity > su.Quantity)
+        //        {
+        //            throw new Exception(
+        //                $"Requested quantity {quantity} > available quantity {su.Quantity} for stock unit: {code}"
+        //            );
+        //        }
+        //    }
+        //}
 
-        private async Task<PicklistItemDto> LoadPickListItem(string pickListCode, string pickListItemCode)
-        {
-            var picklistDto = await _picklistService.GetByCodeAsync(pickListCode);
-            var item = picklistDto.pickListItemList.FirstOrDefault(i => i.code == pickListItemCode);
+        //private async Task<PicklistItemDto> LoadPickListItem(string pickListCode, string pickListItemCode)
+        //{
+        //    var picklistDto = await _picklistService.GetByCodeAsync(pickListCode);
+        //    var item = picklistDto.pickListItemList.FirstOrDefault(i => i.code == pickListItemCode);
 
-            if(item.State != PicklistItemState.OPEN)
-            {
-                throw new Exception("PickListItem is not OPEN: " + item.State);
-            }
+        //    if(item.State != PicklistItemState.OPEN)
+        //    {
+        //        throw new Exception("PickListItem is not OPEN: " + item.State);
+        //    }
 
-            return item;
-        }
+        //    return item;
+        //}
 
     }
 }
---
+++ dotnet_Warehouse-Management-System.Tests/ProductsTests/ProductServiceTests.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using dotnet_Warehouse_Management_System.Products.Entities.Services;
+using dotnet_Warehouse_Management_System.Products.Entities.Repository;
+using dotnet_Warehouse_Management_System.Products.Entities.Dtos;
+using dotnet_Warehouse_Management_System.Products.Entities.Mappers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dotnet_Warehouse_Management_System.Products.Entities;
+
+namespace dotnet_Warehouse_Management_System.ProductsTests
+{
+    public class ProductServiceTests
+    {
+        private readonly Mock<IProductRepository> _repoMock;
+        private readonly ProductService _service;
+
+        public ProductServiceTests()
+        {
+            _repoMock = new Mock<IProductRepository>();
+            _service = new ProductService(_repoMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnMappedProducts()
+        {
+            // Arrange
+            var products = new List<Product>
+            {
+                new Product { Name = "Prod1" },
+                new Product { Name = "Prod2" }
+            };
+            _repoMock.Setup(r => r.GetAllAsync())
+                     .ReturnsAsync(products);
+
+            // Act
+            var result = await _service.GetAllAsync();
+
+            // Assert
+            result.Should().HaveCount(2);
+            result[0].Name.Should().Be("Prod1");
+            result[1].Name.Should().Be("Prod2");
+        }
+
+        [Fact]
+        public async Task GetByCodeAsync_ProductExists_ShouldReturnDto()
+        {
+            // Arrange
+            var product = new Product { Name = "Prod1" };
+            _repoMock.Setup(r => r.GetByCodeAsync("CODE1"))
+                     .ReturnsAsync(product);
+
+            // Act
+            var result = await _service.GetByCodeAsync("CODE1");
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Name.Should().Be("Prod1");
+        }
+
+        [Fact]
+        public async Task GetByCodeAsync_ProductNotExists_ShouldReturnNull()
+        {
+            // Arrange
+            _repoMock.Setup(r => r.GetByCodeAsync("UNKNOWN"))
+                     .ReturnsAsync((Product?)null);
+
+            // Act
+            var result = await _service.GetByCodeAsync("UNKNOWN");
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldCallGenerateCodeAndReturnDto()
+        {
+            // Arrange
+            var dto = new ProductRequestDto { Name = "Nuovo" };
+            var created = dto.ToProduct(); // assume GenerateCode viene chiamato internamente
+            _repoMock.Setup(r => r.CreateAsync(It.IsAny<Product>()))
+                     .ReturnsAsync(created);
+
+            // Act
+            var result = await _service.CreateAsync(dto);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be("Nuovo");
+            _repoMock.Verify(r => r.CreateAsync(It.IsAny<Product>()), Times.Once);
+        }
+    }
+}
